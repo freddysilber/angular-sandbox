f0198fefa2375703e445e9d8b37aac90aeea0df5
Freddy Silber <freddy.silber@cloudcoach.com>
2020-06-11 10:12:20 -0600

    adds server component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms'
+
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -7,7 +9,8 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
