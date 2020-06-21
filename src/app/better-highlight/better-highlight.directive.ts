659f298e80f5c57cc843ee5db5d09c0b02fd423d
Freddy Silber <freddy.silber@cloudcoach.com>
2020-06-21 15:33:12 -0600

    adds comments

--- src/app/better-highlight/better-highlight.directive.ts
+++ src/app/better-highlight/better-highlight.directive.ts
@@ -8,12 +8,14 @@ export class BetterHighlightDirective implements OnInit {
   constructor(private elRef: ElementRef, private renderer: Renderer2) { }
 
   ngOnInit() {
-    // this.renderer.setStyle(this.elRef.nativeElement, 'background-color', '#666')
+    // On render, set background to red
+    this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'red')
   }
-
+  // onhover, change background color
   @HostListener('mouseenter') mouseover(eventData: Event) {
     this.renderer.setStyle(this.elRef.nativeElement, 'background-color', '#666')
   }
+  // onmouse leave, change background again
   @HostListener('mouseleave') mouseleave(eventData: Event) {
     this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'lightgreen')
   }
