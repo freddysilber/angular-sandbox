e3e748ba9edb8306ece311ab23fc7b5d277961d9
freddysilber <freddy.silber@gmail.com>
2020-06-24 21:11:25 -0600

    removes old code

--- src/app/better-highlight/better-highlight.directive.ts
+++ src/app/better-highlight/better-highlight.directive.ts
@@ -9,18 +9,14 @@ export class BetterHighlightDirective implements OnInit {
   constructor(private elRef: ElementRef, private renderer: Renderer2) { }
 
   ngOnInit() {
-    // On render, set background to red
-    // this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'red')
     this.backgroundColor = 'red'
   }
   // onhover, change background color
   @HostListener('mouseenter') mouseover(eventData: Event) {
-    // this.renderer.setStyle(this.elRef.nativeElement, 'background-color', '#666')
     this.backgroundColor = '#666'
   }
   // onmouse leave, change background again
   @HostListener('mouseleave') mouseleave(eventData: Event) {
-    // this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'lightgreen')
     this.backgroundColor = 'lightgreen'
   }
 }
