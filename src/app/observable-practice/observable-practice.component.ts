e227c2d2f1c84c1a39b7737ffe09b2efd0a7563e
freddysilber <freddy.silber@gmail.com>
2020-08-30 11:39:33 -0600

    notes

--- src/app/observable-practice/observable-practice.component.ts
+++ src/app/observable-practice/observable-practice.component.ts
@@ -20,7 +20,44 @@ function sequenceSubscriber(observer) {
 
   return { unsubscribe() { } }
 }
+// START GEOLOCATION OBSERVABLE (FROM ANGULAR DOCS)
+// Create an Observable that will start listening to geolocation updates
+// when a consumer subscribes.
+const locations = new Observable((observer) => {
+  let watchId: number
+  // Simple geolocation API check provides values to publish
+  if ('geolocation' in navigator) {
+    watchId = navigator.geolocation.watchPosition((position: Position) => {
+      observer.next(position)
+    }, (error: PositionError) => {
+      observer.error(error)
+    })
+  } else {
+    observer.error('Geolocation not available')
+  }
+  // When the consumer unsubscribes, clean up data ready for next subscription.
+  return {
+    unsubscribe() {
+      navigator.geolocation.clearWatch(watchId)
+    }
+  }
+})
 
+// Call subscribe() to start listening for updates.
+const locationsSubscription = locations.subscribe({
+  next(position) {
+    console.log('Current Position: ', position)
+  },
+  error(msg) {
+    console.log('Error Getting Location: ', msg)
+  }
+})
+
+// Stop listening for location after 10 seconds
+setTimeout(() => {
+  locationsSubscription.unsubscribe()
+}, 10000)
+// END GEOLOCATION OBSERVABLE
 @Component({
   selector: 'app-observable-practice',
   templateUrl: './observable-practice.component.html',
@@ -52,7 +89,9 @@ export class ObservablePracticeComponent implements OnInit, OnDestroy {
     if (this.timerSub) { // Try to prevent user to start multiple timers 
       return
     }
-    this.timerSub = timer(0, 1000).subscribe(val => this.timerValue = val)
+    this.timerSub = timer(0, 1000).subscribe(val => {
+      this.timerValue = val
+    })
   }
 
   stopTimer(): void {
