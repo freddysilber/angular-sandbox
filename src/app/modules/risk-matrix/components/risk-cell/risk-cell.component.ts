ca4638294e571ffce73de8e40d46346ede1893c4
freddysilber <freddy.silber@gmail.com>
2020-12-19 16:47:28 -0700

    coloring matrix

--- src/app/modules/risk-matrix/components/risk-cell/risk-cell.component.ts
+++ src/app/modules/risk-matrix/components/risk-cell/risk-cell.component.ts
@@ -7,4 +7,13 @@ import { Component, Input } from '@angular/core'
 })
 export class RiskCellComponent {
 	@Input('value') value
+	@Input('row') row
+	@Input('column') column
+
+	get isGreen() {
+		console.log(this.row, this.column)
+		if (this.column === 0 && this.row >= 2 && this.row <= 4 || this.row === 4 && this.column <= 2 || this.row === 3 && this.column === 1) {
+			return true
+		}
+	}
 }
