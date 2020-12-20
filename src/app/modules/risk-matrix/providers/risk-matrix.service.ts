76219fd1d9ba6e7e72dc8e90116c2fd6940f96d2
freddysilber <freddy.silber@gmail.com>
2020-12-20 16:16:30 -0700

    refactoring

--- src/app/modules/risk-matrix/providers/risk-matrix.service.ts
+++ src/app/modules/risk-matrix/providers/risk-matrix.service.ts
@@ -3,27 +3,26 @@ import { BehaviorSubject } from 'rxjs'
 
 import { Ticket, Matrix } from '../models'
 
-const RISK_MATRIX_DIMENSIONS = 5
+const RISK_MATRIX_DIMENSIONS: number = 5
+const DATA: Ticket[] = [
+	{ name: 'OneOne', impact: 1, probability: 1 },
+	{ name: 'Name', impact: 1, probability: 4 },
+	{ name: 'First', impact: 1, probability: 4 },
+	{ name: 'Second', impact: 2, probability: 5 },
+	{ name: 'Third', impact: 5, probability: 3 },
+	{ name: 'Fourth', impact: 4, probability: 1 },
+	{ name: 'Center', impact: 3, probability: 3 },
+	{ name: 'FiveFive', impact: 5, probability: 5 },
+]
 
 @Injectable({
 	providedIn: 'root'
 })
 export class RiskMatrixService {
-
+	private _RISK_MATRIX_DIMENSIONS: number = RISK_MATRIX_DIMENSIONS
+	private _sampleData: Ticket[] = DATA
 	canSelectMultiple: boolean = false
 	selectedEmitter: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(false)
-
-	private _RISK_MATRIX_DIMENSIONS: number = RISK_MATRIX_DIMENSIONS
-	private _sampleData: Ticket[] = [
-		{ name: 'OneOne', impact: 1, probability: 1 },
-		{ name: 'Name', impact: 1, probability: 4 },
-		{ name: 'First', impact: 1, probability: 4 },
-		{ name: 'Second', impact: 2, probability: 5 },
-		{ name: 'Third', impact: 5, probability: 3 },
-		{ name: 'Fourth', impact: 4, probability: 1 },
-		{ name: 'Center', impact: 3, probability: 3 },
-		{ name: 'FiveFive', impact: 5, probability: 5 },
-	]
 
 	get matrixDimensions(): number {
 		return this._RISK_MATRIX_DIMENSIONS
