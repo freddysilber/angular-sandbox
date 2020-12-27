19f499ce80e49d0bd584fd0478c9264c3ca9d3ae
freddysilber <freddy.silber@gmail.com>
2020-12-27 14:54:07 -0700

    moves data to data file

---
+++ src/app/modules/risk-matrix/providers/risk-matrix-data.ts
@@ -0,0 +1,20 @@
+import { Ticket } from '../models'
+
+export const DATA: Ticket[] = [
+	{ name: 'OneOne', impact: 1, probability: 1 },
+	{ name: 'TwoTwo', impact: 2, probability: 2 },
+	{ name: 'Name', impact: 1, probability: 4 },
+	{ name: 'First', impact: 1, probability: 4 },
+	{ name: 'First', impact: 1, probability: 5 },
+	{ name: 'ThreeFour', impact: 3, probability: 4 },
+	{ name: 'ThreeFour', impact: 3, probability: 4 },
+	{ name: 'ThreeFour', impact: 3, probability: 4 },
+	{ name: 'ThreeFour', impact: 3, probability: 4 },
+	{ name: 'Second', impact: 2, probability: 5 },
+	{ name: 'Third', impact: 5, probability: 3 },
+	{ name: 'Fourth', impact: 4, probability: 1 },
+	{ name: 'FourFout', impact: 4, probability: 4 },
+	{ name: 'Center', impact: 3, probability: 3 },
+	{ name: 'FiveFive', impact: 5, probability: 5 },
+	{ name: 'FiveFive', impact: 5, probability: 5 },
+]
--- src/app/modules/risk-matrix/providers/risk-matrix.service.ts
+++ src/app/modules/risk-matrix/providers/risk-matrix.service.ts
@@ -2,25 +2,7 @@ import { Injectable } from '@angular/core'
 import { BehaviorSubject } from 'rxjs'
 
 import { Ticket, Matrix } from '../models'
-
-const DATA: Ticket[] = [
-	{ name: 'OneOne', impact: 1, probability: 1 },
-	{ name: 'TwoTwo', impact: 2, probability: 2 },
-	{ name: 'Name', impact: 1, probability: 4 },
-	{ name: 'First', impact: 1, probability: 4 },
-	{ name: 'First', impact: 1, probability: 5 },
-	{ name: 'ThreeFour', impact: 3, probability: 4 },
-	{ name: 'ThreeFour', impact: 3, probability: 4 },
-	{ name: 'ThreeFour', impact: 3, probability: 4 },
-	{ name: 'ThreeFour', impact: 3, probability: 4 },
-	{ name: 'Second', impact: 2, probability: 5 },
-	{ name: 'Third', impact: 5, probability: 3 },
-	{ name: 'Fourth', impact: 4, probability: 1 },
-	{ name: 'FourFout', impact: 4, probability: 4 },
-	{ name: 'Center', impact: 3, probability: 3 },
-	{ name: 'FiveFive', impact: 5, probability: 5 },
-	{ name: 'FiveFive', impact: 5, probability: 5 },
-]
+import { DATA } from '../providers/risk-matrix-data'
 
 @Injectable({
 	providedIn: 'root'
