e99f7cac046383df3c5d8e3a6ca70ebd74114a93
freddysilber <freddy.silber@gmail.com>
2021-04-04 17:16:36 -0600

    module stuff

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,21 +6,22 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 import { HttpClientModule } from '@angular/common/http';
 // Components
 import { AppComponent } from './app.component'
-import { ServerComponent } from './components/server/server.component'
-import { ServersComponent } from './components/servers/servers.component'
-import { WarningAlertComponent } from './components/alerts/warning-alert/warning-alert.component'
-import { SuccessAlertComponent } from './components/alerts/success-alert/success-alert.component'
-import { CockpitComponent } from './components/cockpit/cockpit.component'
-import { DashboardComponent } from './components/dashboard/dashboard.component'
-import { UserComponent } from './components/user/user.component'
-import { ReactiveFormComponent } from './components/reactive-form/reactive-form.component'
-import { ExploringPipesComponent } from './components/exploring-pipes/exploring-pipes.component'
-import { AnimationsPracticeComponent } from './components/animations-practice/animations-practice.component'
-import { ObservablePracticeComponent } from './components/observable-practice/observable-practice.component';
-import { NavigationComponent } from './components/navigation/navigation.component'
 import {
 	CatFactsComponent,
-	ScssExploreComponent
+	ScssExploreComponent,
+	ServerComponent,
+	ServersComponent,
+	WarningAlertComponent,
+	SuccessAlertComponent,
+	CockpitComponent,
+	DashboardComponent,
+	UserComponent,
+	ReactiveFormComponent,
+	ExploringPipesComponent,
+	AnimationsPracticeComponent,
+	ObservablePracticeComponent,
+	NavigationComponent
+
 } from './components';
 // Services
 import { MESSAGING_TOKEN, MessagingService } from './services/messaging.service'
@@ -43,29 +44,31 @@ const COMPONENTS = [
 	AppComponent,
 	CatFactsComponent,
 	ScssExploreComponent,
-	ServerComponent
+	ServerComponent,
+	ServersComponent,
+	WarningAlertComponent,
+	SuccessAlertComponent,
+	CockpitComponent,
+	DashboardComponent,
+	UserComponent,
+	ReactiveFormComponent,
+	ExploringPipesComponent,
+	AnimationsPracticeComponent,
+	ObservablePracticeComponent,
+	NavigationComponent
 ]
 
 @NgModule({
 	declarations: [
-		ServersComponent,
-		WarningAlertComponent,
-		SuccessAlertComponent,
-		CockpitComponent,
 		BasicHighlightDirective,
 		BetterHighlightDirective,
 		UnlessDirective,
 		ComponentPlaceholderDirective,
-		DashboardComponent,
-		UserComponent,
-		ReactiveFormComponent,
-		ExploringPipesComponent,
-		AnimationsPracticeComponent,
-		COMPONENTS,
+
 		ShortenPipe,
 		FilterPipe,
-		ObservablePracticeComponent,
-		NavigationComponent
+
+		COMPONENTS,
 	],
 	imports: [
 		BrowserModule,
