9434ccffc1678fde1e3f639ad86ba500f90105d2
freddysilber <freddy.silber@gmail.com>
2021-06-17 19:14:45 -0600

    ;

--- src/app/services/messaging.service.ts
+++ src/app/services/messaging.service.ts
@@ -1,17 +1,17 @@
 import { Injectable, InjectionToken } from '@angular/core';
 
 export interface Message {
-	messageName: string,
-	sendMessage(message: string): void
+	messageName: string;
+	sendMessage(message: string): void;
 }
 
 export const MESSAGING_TOKEN = new InjectionToken<Message>('messaging');
 
 @Injectable()
 export class MessagingService implements Message {
-	public messageName: string = 'MessagingService';
+	public messageName = 'MessagingService';
 
 	public sendMessage(message: string): string {
-		return `Message: ${message} \n From: MessagingService`
+		return `Message: ${message} \n From: MessagingService`;
 	}
 }
--- src/app/services/util.service.ts
+++ src/app/services/util.service.ts
@@ -1,16 +1,16 @@
 import { Injectable } from '@angular/core';
 
 @Injectable({
-  providedIn: 'root'
+	providedIn: 'root'
 })
 export class UtilService {
-  colors = {
-    red: 'red',
-    green: 'green',
-    lightgreen: 'lightgreen',
-    blue: 'blue',
-    orange: 'orange'
-  }
+	colors = {
+		red: 'red',
+		green: 'green',
+		lightgreen: 'lightgreen',
+		blue: 'blue',
+		orange: 'orange'
+	}
 
-  constructor() { }
+	constructor() { }
 }
