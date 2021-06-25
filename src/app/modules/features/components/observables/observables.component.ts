577b30e2af808e189b1adbd1c9d33715d519644b
freddysilber <freddy.silber@gmail.com>
2021-06-24 19:24:59 -0600

    /

--- src/app/modules/features/components/observables/observables.component.ts
+++ src/app/modules/features/components/observables/observables.component.ts
@@ -1,8 +1,8 @@
-import { Component, OnDestroy, OnInit } from '@angular/core'
-import { from, interval, Observable, Subscription } from 'rxjs'
-import { take, map, skip } from 'rxjs/operators'
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { from, interval, Observable, Subscription } from 'rxjs';
+import { take, map, skip } from 'rxjs/operators';
 
-import { ObservablesService } from '../../providers'
+import { ObservablesService } from '../../providers';
 
 @Component({
 	selector: 'app-observables',
@@ -10,51 +10,51 @@ import { ObservablesService } from '../../providers'
 	styleUrls: ['./observables.component.scss']
 })
 export class ObservablesComponent implements OnInit, OnDestroy {
-	count: number
-	incrementOutput: string[] = []
-	inputPlaceholder: string
-	private _sub: Subscription
-	private _incrementSub: Subscription
+	count: number;
+	incrementOutput: string[] = [];
+	inputPlaceholder: string;
+	private _sub: Subscription;
+	private _incrementSub: Subscription;
 
 	constructor(
 		private _observablesService: ObservablesService
 	) {
-		this.inputPlaceholder = 'abcdefghijklmnopqrstuvwxyz'
-		this.count = 0
+		this.inputPlaceholder = 'abcdefghijklmnopqrstuvwxyz';
+		this.count = 0;
 	}
 
 	ngOnInit(): void {
 		this._incrementSub = this._observablesService.incrementEmitter$.pipe(
 			map((value: number) => {
 				if (typeof value === 'number') {
-					return value % 2 === 0 ? `Even: ${value}` : `Odd: ${value}`
+					return value % 2 === 0 ? `Even: ${value}` : `Odd: ${value}`;
 				}
-				return value
+				return value;
 			})
 		).subscribe((value: string) => {
-			this.incrementOutput.push(value)
-		})
+			this.incrementOutput.push(value);
+		});
 	}
 
 	ngOnDestroy(): void {
 		if (this._observablesService.customIntervalSubscription) {
-			this._observablesService.customIntervalSubscription.unsubscribe()
+			this._observablesService.customIntervalSubscription.unsubscribe();
 		}
 		if (this._incrementSub) {
-			this._incrementSub.unsubscribe()
+			this._incrementSub.unsubscribe();
 		}
 	}
 
 	increment(): void {
-		this._observablesService.incrementEmitter$.next(this.count++)
+		this._observablesService.incrementEmitter$.next(this.count++);
 	}
 
 	exploreObservables(): void {
-		this._observablesService.exploreObservables()
+		this._observablesService.exploreObservables();
 	}
 
 	startCustomObservable(): void {
-		this._observablesService.startCustomObservable()
+		this._observablesService.startCustomObservable();
 	}
 
 	processIncrementOutput(): void {
@@ -62,8 +62,8 @@ export class ObservablesComponent implements OnInit, OnDestroy {
 			skip(2),
 			take(3),
 			map((value: string) => `this is a keeper: ${value}`)
-		)
-		incrementOutput.subscribe((value: string) => console.log(value)).unsubscribe()
+		);
+		incrementOutput.subscribe((value: string) => console.log(value)).unsubscribe();
 	}
 
 	processAlphabet(event): void {
@@ -71,18 +71,18 @@ export class ObservablesComponent implements OnInit, OnDestroy {
 			skip(1),
 			take(15),
 			map((letter: string) => 'letter: ' + letter)
-		)
+		);
 
 		alphabetObservable.subscribe((letter: string) => {
-			console.log(letter)
-		}).unsubscribe()
+			console.log(letter);
+		}).unsubscribe();
 
 		this._sub = interval(100).subscribe(count => {
 			if (!event.value[count]) {
-				this._sub.unsubscribe()
+				this._sub.unsubscribe();
 			} else {
-				console.log(event.value[count])
+				console.log(event.value[count]);
 			}
-		})
+		});
 	}
 }
